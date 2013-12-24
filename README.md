15727bccf0749cc64a28614ced963e6d8fdbc67a
Matthijs Alles <admin@bixie.nl>
2013-12-24 21:05:12 +0100

    log en tablegroups

--- README.md
+++ README.md
@@ -3,4 +3,13 @@ Bixie Printshop Cronjobs
 
 Cronjobs voor Bixie Printshop
 
-_Bestanen buiten webroot server!_
+_Bestanden buiten webroot server!_
+
+Dagelijkse complete backup:
+```
+01	5	*	*	*	/usr/local/bin/php /home/crmprescan/domains/virtueupload.org/cronjobs/cronjobs.php dumpdb -f daily	
+```
+Vijfmaal daags belangrijkste tabellen
+```
+01	8,12,15,18,23	*	*	*	/usr/local/bin/php /home/crmprescan/domains/virtueupload.org/cronjobs/cronjobs.php dumpdb -m 5 -t freq
+```
--- cronjobs/cronjobs.php
+++ cronjobs/cronjobs.php
@@ -18,7 +18,7 @@ switch ($task) {
 		$cronClass->dumpdb();
 	break;
 	default:
-		printf('Task %s niet gevonden!'._N_,$task);
+		BixiePrintShopCronJobs::log(sprintf('Task %s niet gevonden!'._N_,$task));
 	break;
 }
 
@@ -29,6 +29,7 @@ class BixiePrintShopCronJobs {
 	private $argv;
 	private static $_DBFOLDER = 'dbdumps';
 	private static $_HTMLFOLDER = 'public_html';
+	private static $_LOGFOLDER = '../public_html/logs/bps_logs';
 
 	public function __construct($argv) {
 		$this->argv = $argv;
@@ -53,16 +54,22 @@ class BixiePrintShopCronJobs {
 	
 	public function dumpdb() {
 		//init vars
-		$aArguments = $this->_getArguments(array('-f'=>'folder','-m'=>'max'));
+		$aArguments = $this->_getArguments(array('-f'=>'folder','-m'=>'max','-t'=>'tablegroup'));
 		$basePath = dirname(__FILE__);
 		$rootPath = str_replace(DS.basename(__DIR__),'',$basePath);
 		$subFolder = isset($aArguments['folder'])? DS.$aArguments['folder']: '';
 		$maxItems = isset($aArguments['max'])?intval($aArguments['max']):14;
+		$tablegroup = !empty($aArguments['tablegroup'])?$aArguments['tablegroup']:'all';
+		$tablegroups = array(
+			'freq'=>'dhn2_bps_order dhn2_bps_orderoption dhn2_bps_orderoptionvalues dhn2_bps_besteladres dhn2_bps_bestelling dhn2_users dhn2_user_profiles dhn2_user_usergroup_map',
+			'all'=>''
+		);
+		$tables = isset($tablegroups[$tablegroup])?$tablegroups[$tablegroup]:'';
 		$sDumpPath = $rootPath.DS.self::$_DBFOLDER.$subFolder;
-		//huidige dumps bekijken
 		if (!file_exists($sDumpPath)) {
 			mkdir($sDumpPath, 0755, true);
 		}
+		//huidige dumps bekijken
 		$allDates = array();
 		if ($handle = opendir($sDumpPath)) {
 			while (false !== ($entry = readdir($handle))) {
@@ -76,21 +83,43 @@ class BixiePrintShopCronJobs {
 		if (count($allDates) > $maxItems - 1) {
 			foreach ($allDates as $file=>$date) {
 				@unlink($sDumpPath.DS.$file);
+				self::log("$file verwijderd."._N_);
 				break;
 			}
 		}
 		$date = new DateTime('NOW',new DateTimeZone('UTC'));
 		$now = $date->format('Ymd-His');
 		$file = $sDumpPath.DS.'dump_'.$now.'.sql.zip';
+		$logFile = $rootPath.DS.self::$_HTMLFOLDER.DS.'dbdumps_'.$date->format('Y-W').'.log';
 		//db-gegevens
 		$webFolder = $rootPath.DS.self::$_HTMLFOLDER;
 		if (file_exists($webFolder.DS.'configuration.php')) {
 			require_once $webFolder.DS.'configuration.php';
 			$oJConfig = new JConfig();
-			system("mysqldump --opt -h {$oJConfig->host} -u {$oJConfig->user} -p{$oJConfig->password} {$oJConfig->db} | zip > {$file}");
+			$command = "mysqldump --opt -h {$oJConfig->host} -u {$oJConfig->user} -p{$oJConfig->password} {$oJConfig->db} {$tables} | zip > {$file}";
+			system($command, $retval);
+			if ($retval == 0) {
+				$sLog = $date->format('d-m-Y H:i:s').": Backup succesvol."._N_;
+				$sLog .= "$command"._N_;
+				self::log($sLog);
+			} else {
+				$sLog = $date->format('d-m-Y H:i:s').": Backup mislukt!."._N_;
+				$sLog .= "$command"._N_;
+				self::log($sLog);
+			}
 		} else {
-			print "Configuratiebestand niet gevonden!"._N_;
+			self::log("Configuratiebestand niet gevonden!"._N_);
 		}
 		die();
+	}
+	
+	public static function log($message) {
+		$date = new DateTime('NOW',new DateTimeZone('UTC'));
+		$logPath = dirname(__FILE__).DS.self::$_LOGFOLDER;
+		if (!file_exists($logPath)) {
+			mkdir($logPath, 0755, true);
+		}
+		$logFile = $logPath.DS.'dbdumps_'.$date->format('Y-W').'.log';
+		@file_put_contents($logFile,$message,FILE_APPEND);
 	}
 }
